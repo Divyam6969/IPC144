dc2b4c65aa6def1ff9804a6fe110ab5e7b519c2f
Divyam <divyammalik2003@gmail.com>
2022-08-13 13:53:02 +0530

    lol

--- cosewic.h
+++ cosewic.h
@@ -23,3 +23,24 @@ int loadData(char* s,FILE *fp,int max)
 
     return count;
 }
+
+int mainLogic()
+{
+    printf("=======================================================\nCanada Species at Risk Analysis\n(Data Years: 2010 - 2019)\n=======================================================\n");
+
+    printf("1. View by year (sorted ASCENDING by year)\n2. View by province (sorted DESCENDING by totals)\n3. View by taxon (sorted ASCENDING by totals)\n4. View by province and taxon\n-------------------------------------------------------\n0. Exit");
+    printf("\n");
+    printf("Selection: ");
+    int n;
+    scanf("%d",&n);
+    
+    if(n<0||n>4)
+    {
+        printf("Invalid menu selection!");
+    }
+    if(n==0)
+    {
+        printf("Application terminated!");
+        return 0;
+    }
+}
--- main.c
+++ main.c
@@ -27,33 +27,17 @@ int main(void)
     records = loadData(DATA_FILE, data, MAX_RECORDS);
     printf("%d data records read from file...\n\n", records);
 
-    // if (records > 0)
-    // {
-    //     // Entry point to application logic
-    //     mainLogic(data, records);
-    // }
-    // else
-    // {
-    //     printf("Ooooops there seems to have been an error loading data!\n");
-    // }
+    if (records > 0)
+    {
+        // Entry point to application logic
+        mainLogic(data, records);
+    }
+    else
+    {
+         printf("Ooooops there seems to have been an error loading data!\n");
+    }
 
-    printf("=======================================================\nCanada Species at Risk Analysis\n(Data Years: 2010 - 2019)\n=======================================================\n");
-
-    printf("1. View by year (sorted ASCENDING by year)\n2. View by province (sorted DESCENDING by totals)\n3. View by taxon (sorted ASCENDING by totals)\n4. View by province and taxon\n-------------------------------------------------------\n0. Exit");
-    printf("\n");
-    printf("Selection: ");
-    int n;
-    scanf("%d",&n);
     
-    if(n<0||n>4)
-    {
-        printf("Invalid menu selection!");
-    }
-    if(n==0)
-    {
-        printf("Application terminated!");
-        return 0;
-    }
     
     return 0;
 }
